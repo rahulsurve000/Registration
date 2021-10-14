5f473b2eb25b07b62cd9738e46789fee23937b3c
Rahul Surve <rksurve1612@gmail.com>
2021-10-14 19:18:30 +0530

    JS edited

--- valid.js
+++ valid.js
@@ -15,10 +15,10 @@ form.addEventListener("submit", function (event) {
     if (!(validateUsername(username.value) && validatePassword(password.value))) {        
         event.preventDefault();
         if(!validateUsername(username.value)){
-            alert("Username rules: Less than 20 characters")
+            alert("Username should be Less than 20 characters")
         }
         if(!validatePassword(password.value)){
-            alert("Password rules: Min length 8 characters. Contains one lowercase letter, one uppercase letter and one special character ")
+            alert("Password should be Min length 8 characters. Contains one lowercase letter, one uppercase letter and one special character ")
         }
     }
     else{
@@ -60,118 +60,3 @@ function validatePassword(password) {
     }
 }
 
-
-
-
-
-// var fn = document.form1.fname.value;
-// var pass1 = document.form1.Password.value;
-// var pass2 = document.form1.ConPassword.value;
-// var dt = document.form1.date.value;
-// var city = document.form1.city.value;
-// var patt = /[^a-z]/i;
-// var fname = patt.test(fn);
-
-
-
-
-
-
-// function fncValidate() {
-
-// 	// var lname = patt.test(ln); 
-
-// 	if (fn == "") {
-// 		alert('Please input Firstname');
-
-// 		return false;
-// 	}
-// 	if (fn == null || fname == true) {
-// 		alert("Enter only Alphabet for Firstname");
-// 		return false;
-// 	}
-// 	// 	if(ln == "")
-// 	// {
-// 	// 	alert('Please input Lastname');
-
-// 	// 	return false;
-// 	// }
-// 	// 		if ( ln == null || lname == true )
-// 	// 		{
-// 	// 			alert("Enter only Alphabet for Lastname");
-// 	// 			return false;
-// 	// 		}
-
-// 	if (pass1 == "") {
-// 		alert('Please input Password');
-
-// 		return false;
-// 	}
-
-// 	if (pass1.length < 6) {
-// 		alert('Password strength should not less than 6 characters');
-
-// 		return false;
-// 	}
-
-
-// 	if (pass2 == "") {
-// 		alert('Please input Confirm Password');
-// 		return false;
-// 	}
-
-// 	if (pass1 != pass2) {
-// 		alert('Confirm Password Not Match');
-// 		return false;
-// 	}
-
-// 	var em = document.form1.email.value;
-// 	var atpos = em.indexOf("@");
-// 	var dotpos = em.lastIndexOf(".");
-// 	if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= em.length) {
-// 		alert("Ivalid email id");
-// 		return false;
-// 	}
-
-// 	var add = document.form1.date.value;
-// 	if (add == "") {
-// 		alert('Please enter your DOB');
-
-// 		return false;
-// 	}
-// 	// if ( add == null || address == true )
-// 	// 		{
-// 	// 			alert("Enter only Alphabet for Address with ,");
-// 	// 			return false;
-// 	// 		}
-
-// 	var c = document.getElementById("city")
-// 	if (c.value == null || c.value == "") {
-// 		alert("Please select a city");
-// 		return false;
-// 	}
-
-
-// 	// var cp = document.getElementById('C');
-// 	// var cpp = document.getElementById('C++');
-// 	// if ((cp.checked == false) && (cpp.checked == false)) {
-// 	// 	alert("Please choose Programming Languages known");
-// 	// 	document.getElementById("plk");
-// 	// 	return false;
-// 	// }
-
-// 	function check() {
-// 		var markedCheckbox = document.getElementsByName('checked');
-// 		for (var checkbox of markedCheckbox) {
-// 		  if (checkbox.checked)
-// 			document.body.append(checkbox.value + ' ');
-// 			console.log(checkbox)
-// 		}
-// 	  }
-	
-
-
-
-// 	alert(`Details: name: ${fn}, ${check()} ${dt}, ${city}`);
-// 	// document.form1.submit();
-// }
